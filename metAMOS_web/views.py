297edadcc97e2642efb67afcdba64179c5a2e99b
krassowski <krassowski.michal@gmail.com>
2015-09-09 17:12:02 +0200

    Code cleaning: Remove currently unused code from views.py

--- metAMOS_web/views.py
+++ metAMOS_web/views.py
@@ -7,53 +7,11 @@ sys.path.append('/home/pszczesny/soft/metAMOS_web_interface/metAMOS_web')
 sys.path.append('/home/pszczesny/soft/metAMOS_web_interface/metAMOS_web_interface')
 
 import daemon
-from paths import OUTPUT_ANALYSES_PATH
 import forms
 import helpers
 import models
 
-#with open('/home/pszczesny/soft/metAMOS_web_interface/metAMOS_web/krona-2.0.js') as f:
-#    KRONA_JS_SRC = f.read()
 
-
-KRONA_HTML_PREFIX = """<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-        <head>
-                <meta charset="utf-8"/>
-                <style>
-                        body
-                        {
-                                margin:0;
-                        }
-                </style>
-        </head>
-
-        <body style="padding:0;position:relative">
-                <div id="options" style="position:absolute;left:0;top:0">
-                </div>
-
-                <div id="details" style="position:absolute;top:1%;right:2%;text-align:right;">
-                </div>
-
-                <canvas id="canvas" width="100%" height="100%">
-                        This browser does not support HTML5 (see
-                        <a href="http://sourceforge.net/p/krona/wiki/Browser%20support/">
-                                Krona browser support</a>).
-                </canvas>
-
-                <img id="hiddenImage" src="http://krona.sourceforge.net/img/hidden.png" visibility="hide"/>
-                <script name="tree" src="http://krona.sourceforge.net/krona-1.1.js"></script>
-        </body>
-
-        <data>
-"""
-
-KRONA_HTML_POSTFIX = """
-</body></data></html>
-"""
-
-
-# Create your views here.
 # main page
 def index(request):
     return render(request, 'index.html')
@@ -174,25 +132,3 @@ def result(request, sample_id, type_of_analysis):
             with open(krona_html_path) as f:
                 html_source = f.read()
         return HttpResponse(html_source, content_type='text/html')
-
-
-
-
-# show files from output_analys
-def show_krona_list(request):
-    krona_list = [i for i in os.listdir(OUTPUT_ANALYSES_PATH) if 'krona' in i]
-    return render(request, 'output_analyses.html', {'krona_list': krona_list})
-
-def show_single_krona(request, krona_name):
-    krona_xml_path = os.path.join(OUTPUT_ANALYSES_PATH, krona_name)
-    krona_html_path = os.path.join(OUTPUT_ANALYSES_PATH, krona_name + '.xml.html')
-    #if os.path.exists(krona_html_path):
-    #    os.remove(krona_html_path)
-    os.system(
-            'PERL5LIB=/home/pszczesny/soft/metAMOS-1.5rc3/Utilities/krona /home/pszczesny/soft/metAMOS-1.5rc3/Utilities/krona/ImportXML.pl' + \
-                    krona_xml_path + ' -o ' + krona_html_path
-            )
-    with open(krona_html_path) as f:
-        html_source = f.readlines()
-    return HttpResponse(html_source, content_type='text/html')
-
