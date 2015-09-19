7904abb0d1381ca292534bc4daaa7b23d31fb9fd
krassowski <krassowski.michal@gmail.com>
2015-09-19 03:14:50 +0200

    Rewritten and cleaned 'paths' module

--- metAMOS_web/paths.py
+++ metAMOS_web/paths.py
@@ -1,18 +1,54 @@
-# file paths
-SAMPLE_PATH = {
-        'TEST': '/home/pszczesny/storage/test_samples',
-        'META_TEST': '/home/pszczesny/storage/test_meta_files',
-        #'ITS': '/home/pszczesny/storage/20140519_data/amp/ITS',
-        #'16S': '/home/pszczesny/storage/20140519_data/amp/16S',
-        }
-OUTPUT_ANALYSES_PATH = '/home/pszczesny/storage/20140519_data/output_analyses/out_all_usearches'
-METAMOS_PATH = '/media/storage/pszczesny/soft/metAMOS-1.5rc3'
-KRONA_PATH = '/media/storage/pszczesny/soft/metAMOS-1.5rc3/KronaTools/'
-WORKFLOWS_PATH = '/media/storage/pszczesny/soft/metAMOS-1.5rc3/Utilities/config'
-TMP_PATH = '/tmp'
-PATH_METAMOS_DIR = '/home/pszczesny/soft/metAMOS-1.5rc3/'
-PATH_METATR_OUT_DIR = '/home/pszczesny/storage/metatranscriptomics'
-# TODO: currently we are using newer version of bipype than one used in metamos
-# after migration - replace this path
-PATH_BIPYPE = '/home/pszczesny/tmp/bipype/bipype'
-PATH_METADATA = '/home/pszczesny/soft/metAMOS_web_interface/biogaz_libraries_metadata_sample.xlsx'
+import os
+import urllib
+import tempfile
+import string
+
+
+def encode(path):
+    return urllib.quote(path, safe='')
+
+
+def decode(path):
+    return urllib.unquote(path)
+
+
+def make_unique(parent_dir):
+        absolute_path = tempfile.mkdtemp(dir=parent_dir)
+        return os.path.basename(absolute_path)
+
+
+def expand(paths_dict):
+    for alias, path in paths_dict.iteritems():
+        path = string.Template(path).substitute(paths_dict)
+        paths_dict[alias] = os.path.expanduser(path)
+    return paths_dict
+
+
+class Paths(object):
+
+    PATHS = {
+        'tmp': '/tmp',
+        'metAMOS': '~/soft/metAMOS-1.5rc3/',
+        'workflows': '$metAMOS/Utilities/config',
+        'storage': '~/storage/metamos_web_interface/',
+        'bipype': '~/soft/bipype_new/bipype',
+        'interface_root': '~/soft/metAMOS_web_interface',
+        'metadata': '$interface_root/biogaz_libraries_metadata_sample.xlsx'
+    }
+
+    def __init__(self):
+        self.PATHS = expand(self.PATHS)
+
+    def __getattr__(self, alias):
+        return self.PATHS[alias]
+
+    def unique_path_for(self, type_of_analysis):
+
+        path = os.path.join(self.storage, type_of_analysis)
+
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+        return make_unique(path)
+
+app_paths = Paths()
